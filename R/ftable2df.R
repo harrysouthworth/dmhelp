eee4cc9f5cad9d1cc778286c44055975cef13f89
Harry Southworth <harry.southworth@gmail.com>
2014-11-04 13:52:59 +0000

    Adding roxygen to ftable2data.frame

--- R/ftable2df.R
+++ R/ftable2df.R
@@ -3,6 +3,20 @@
 # Author: Tim Bergsma
 # URL: http://metrumrg.googlecode.com
 
+#' Convert ftable to data.frame
+#' @description Convert ftable to data.frame As Displayed
+#' @param x ftable
+#' @param ... ignored
+#' @details \code{as.data.frame} does indeed convert an \code{ftable} to \code{data.frame},
+#'          but it gives a stacked result. Use this function to give something more like
+#'          what is displayed at the prompt (suitable for passing to latex).
+#' @author Tim Bergsma
+#' @return data.frame
+#' @references http://metrumrg.googlecode.com
+#' @examples ftable(mtcars[c("cyl", "vs", "am", "gear")])
+#' as.data.frame(ftable(mtcars[c("cyl", "vs", "am", "gear")]))
+#' ftable2data.frame(ftable(mtcars[c("cyl", "vs", "am", "gear")]))
+#' @export ftable2.data.frame
 ftable2data.frame <- function(x,...){
   y <- format(x,quote=FALSE)
   z <- data.frame(y[-1,],stringsAsFactors=FALSE)
