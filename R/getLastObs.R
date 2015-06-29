ec3d7524b819c03b510153febfb316c102ccb0e5
Harry Southworth <harry.southworth@gmail.com>
2015-06-29 11:10:54 +0100

    Debugging getLastObs

--- R/getLastObs.R
+++ R/getLastObs.R
@@ -13,16 +13,19 @@
 #'   observation per observational unit.
 #' @export getLastObs
 getLastObs <- function(data, id="subject", time="studyday", test="test"){
-  if (any(is.na(data[, subject] | is.na(data[, time]))))
+  if (any(is.na(data[, id]) | is.na(data[, time])))
     stop("Missing values in one of id or time")
 
-  if (length(unique(test)) > 1)
+  if (length(unique(data[, test])) > 1)
     stop("You haven't subset on test")
   
   data <- data[order(data[, time]), ]
   data <- data[order(data[, id]), ]
   
-  res <- data[cumsum(rle(data[, id])$lengths), ]
+  if (is.factor(data[, id]))
+    res <- data[cumsum(rle(as.character(data[, id]))$lengths), ]
+  else
+    res <- data[cumsum(rle(data[, id])$lengths), ]
 
   if (nrow(res) == 0) stop("Result has 0 rows!")
   else if (nrow(res) >= nrow(data)) stop("Result has greater or equal number of rows to the input!")
